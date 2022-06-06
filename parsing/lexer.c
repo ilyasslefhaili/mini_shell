7031414e8d48460a4fd0139e7c99dde4f3ec79e3
ilyasslefhail <ilyasslefhaili15@gmail.com>
2022-06-06 11:49:11 +0100

    number after $

--- parsing/lexer.c
+++ parsing/lexer.c
@@ -156,6 +156,11 @@ char	*ft_after_dollar(t_lexer *lexer, t_list *env_list)
 		str = ft_itoa(get_exit_code());
 		ft_advance(lexer);
 	}
+	else if (ft_isalnum(lexer->c) != 0)
+	{
+		ft_advance(lexer);
+		return (ft_strdup(""));
+	}
 	else
 	{
 		s = ft_strdup("");
